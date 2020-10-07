82a94282de043d2929ed043e1c421992a31d2c99
jakelowen <jakelowen@gmail.com>
2020-10-07 14:48:21 -0500

    fixing url, logging incoming

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ const MessagingResponse = require("twilio").twiml.MessagingResponse;
 const app = express();
 
 app.post("/sms", (req, res) => {
+  // Access the message body and the number it was sent from.
+  console.log(`Incoming message from ${req.body.From}: ${req.body.Body}`);
+
   const url = `https://maker.ifttt.com/trigger/${process.env.IFTTT_EVENT_NAME}/with/key/${process.env.IFTTT_WEBHOOK_KEY}`;
-  console.log({ url });
-  //   // post
-  //   axios
-  //     .post(url)
-  //     .then((res) => {
-  //       console.log(`statusCode: ${res.statusCode}`);
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
+  //   console.log({ url });
+
+  // post
+  axios
+    .post(url)
+    .then((res) => {
+      console.log(`statusCode: ${res.statusCode}`);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 
   // formulate message
   const twiml = new MessagingResponse();
