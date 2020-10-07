c32c144c697f7b7f02c6b9b3f159d2558ad02d99
jakelowen <jakelowen@gmail.com>
2020-10-07 13:57:27 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const express = require("express");
+const axios = require("axios");
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
+
+const app = express();
+
+app.post("/sms", (req, res) => {
+  // post
+  axios
+    .post(
+      "https://maker.ifttt.com/trigger/twilio_message_received/with/key/cEfexBfvpjAknWme7uscR"
+    )
+    .then((res) => {
+      console.log(`statusCode: ${res.statusCode}`);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
+  // formulate message
+  const twiml = new MessagingResponse();
+
+  twiml.message("The Robots are coming! Head for the hills!");
+
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(twiml.toString());
+});
+
+http.createServer(app).listen(1337, () => {
+  console.log("Express server listening on port 1337");
+});
